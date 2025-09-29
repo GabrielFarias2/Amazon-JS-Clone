320a1cd5ee9cc7ca4c5ba963987265669f89c2e9
GabrielFarias2 <gabrielfarias.github@gmail.com>
2025-09-28 21:02:03 -0300

    feat: salva data e gera o html do order summary

--- javascript/checkout/paymentSummary.js
+++ javascript/checkout/paymentSummary.js
@@ -1,6 +1,9 @@
 import { cart } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js"; 
+import {formatCurrency} from "../ultility/money.js";
+
+
 
 export function renderPaymentSummary() {
   let productPriceCents = 0;
@@ -14,6 +17,56 @@ export function renderPaymentSummary() {
     ShippingPriceCents += deliveryOption.priceCents
   });
 
-  console.log(productPriceCents);
-  console.log(ShippingPriceCents);
-} 
+  const totalBeforeTaxCents = productPriceCents + ShippingPriceCents;
+  const taxCents = totalBeforeTaxCents * 0.1;
+  const totalCents = totalBeforeTaxCents + taxCents;
+
+
+  const paymentSummaryHTML = `
+    <div class="payment-summary-title">
+            Order Summary
+          </div>
+
+          <div class="payment-summary-row">
+            <div>Items (3):</div>
+            <div class="payment-summary-money">
+              $${formatCurrency(productPriceCents)}
+            </div>
+          </div>
+
+          <div class="payment-summary-row">
+            <div>Shipping &amp; handling:</div>
+            <div class="payment-summary-money">
+              $${formatCurrency(ShippingPriceCents)}
+            </div>
+          </div>
+
+          <div class="payment-summary-row subtotal-row">
+            <div>Total before tax:</div>
+            <div class="payment-summary-money">
+              $${formatCurrency(totalBeforeTaxCents)}
+            </div>
+          </div>
+
+          <div class="payment-summary-row">
+            <div>Estimated tax (10%):</div>
+            <div class="payment-summary-money">
+              $${formatCurrency(taxCents)}
+            </div>
+          </div>
+
+          <div class="payment-summary-row total-row">
+            <div>Order total:</div>
+            <div class="payment-summary-money">
+            $${formatCurrency(totalCents)}
+            </div>
+          </div>
+
+          <button class="place-order-button button-primary">
+            Place your order
+          </button>
+  `;
+  
+  document.querySelector('.js-payment-summary')
+    .innerHTML = paymentSummaryHTML;
+};
