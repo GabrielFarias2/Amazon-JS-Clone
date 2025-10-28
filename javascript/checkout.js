a7dd4c6e955e550040844d91bec90018436c1a4e
GabrielFarias2 <gabrielfarias.github@gmail.com>
2025-10-28 15:31:20 -0300

    feat: aplicando promisses no back end, promisses sao melhores que callbacks

--- javascript/checkout.js
+++ javascript/checkout.js
@@ -1,9 +1,34 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js';
 // import '../data/backend-pratice.js';
+
+
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+    resolve('value 1');
+});
+}),
+ new Promise((resolve) => {
+    loadCart(() => {
+    resolve('value 2');
+});
+})
+
+]).then((values) => {
+  console.log(values)
+  renderOrderSummary();
+  renderPaymentSummary();
+})
+
+
+
+/*
 loadProducts(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
