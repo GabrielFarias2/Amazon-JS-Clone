d1c56b3672aca69f2741cd782d885468484578af
GabrielFarias2 <gabrielfarias.github@gmail.com>
2025-11-07 10:40:20 -0300

    feat: reorganização de testes e ajustes

--- final-pratice-lesson-18/final-18.js
+++
@@ -1,8 +0,0 @@
-import { XHR } from "../final-pratice-lesson-18/XMLHttpRequest.js";
-import { getGreeting, postGreeting, getGreetingComErro, postInvalido } from "../final-pratice-lesson-18/async.js";
-
-XHR();
-getGreeting();
-postGreeting();
-getGreetingComErro();
-postInvalido();
